cace893cf654aeb3d1499dd0094f5619f693038a
prasanna <prasannakp227@gmail.com>
2017-06-07 22:45:58 -0500

    DisneyProgram

---
+++ Ticket.java
@@ -0,0 +1,15 @@
+package com.Disney;
+
+import Yashu.Task1;
+
+public class Ticket {
+
+	public static void main(String[] args) throws InterruptedException {
+		Task1 task1 = new Task1();
+		task1.open();
+		pathfordisney Pathfordisney=new pathfordisney();
+		Pathfordisney.xpath();
+
+	}
+
+}
