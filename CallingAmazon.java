66b7b458b87387d8dd4d9ddbf1ed69a9e461ebfb
prasanna <prasannakp227@gmail.com>
2017-06-11 22:48:51 -0500

    TestNG practice

---
+++ CallingAmazon.java
@@ -0,0 +1,14 @@
+package com.TestNG;
+
+
+import org.testng.annotations.Test;
+
+public class  CallingAmazon
+{
+	@Test
+	public void test1(){
+		Practce_1 practce_1= new Practce_1();
+		practce_1.saturday(); 
+	}
+}
+
---
+++ Practce_1.java
@@ -0,0 +1,18 @@
+package com.TestNG;
+
+import org.testng.annotations.Test;
+
+public class Practce_1 
+{
+	@Test(priority=1,description="april")
+	public void saturday(){
+		
+		System.out.println("today Saturday");
+	}
+
+		
+	
+
+	
+
+}
